5af25355312a222f50478d697d601d186012e5bd
alyhoop <alyhoop@users.noreply.github.com>
2020-07-06 23:19:48 +0000

    Deploying to master from  @ 3b5f0a0752cb39affe2fca08c24dea8ce2daa9f2 🚀

---
+++ precache-manifest.c4920a99a7bb39150abf91cb6ce3e8ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b703a9506966b6a144183ce4c02e671",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30466bd9db2368316b80",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "df4b8f102fdc0bb821ca",
+    "url": "/static/css/main.fb22b2e7.chunk.css"
+  },
+  {
+    "revision": "30466bd9db2368316b80",
+    "url": "/static/js/2.c99a90b7.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.c99a90b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df4b8f102fdc0bb821ca",
+    "url": "/static/js/main.092ded99.chunk.js"
+  },
+  {
+    "revision": "f6fac08a1749a9dbaa22",
+    "url": "/static/js/runtime-main.a106779c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.67653769c4d107c2fc3570e6f30c22ac.js"
+  "/precache-manifest.c4920a99a7bb39150abf91cb6ce3e8ce.js"
 );
 
 self.addEventListener('message', (event) => {
