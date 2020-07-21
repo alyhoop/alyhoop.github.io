76e9cf025c31e64ebf488684fbf73a8ad7a479f6
alyhoop <alyhoop@users.noreply.github.com>
2020-07-21 19:28:00 +0000

    Deploying to master from  @ 5e9aa26829b6eed4f0ea8a8a3de0102fd0ec2f67 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c4920a99a7bb39150abf91cb6ce3e8ce.js"
+  "/precache-manifest.8ec4166d66767ccd8ba24d9539a8af03.js"
 );
 
 self.addEventListener('message', (event) => {
